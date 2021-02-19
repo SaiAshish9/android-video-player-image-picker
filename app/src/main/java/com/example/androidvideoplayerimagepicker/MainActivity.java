6655b867f3411dc7a6e9135e007b56f7732755ac
saiashish9 <saiashish3760@gmail.com>
2021-02-19 22:18:33 +0530

    completed

--- app/src/main/java/com/example/androidvideoplayerimagepicker/MainActivity.java
+++ app/src/main/java/com/example/androidvideoplayerimagepicker/MainActivity.java
@@ -1,18 +1,58 @@
 package com.example.androidvideoplayerimagepicker;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.VideoView;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_CAMERA_CAPTURE =1;
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn=(Button) findViewById(R.id.button);
+        imageView=(ImageView) findViewById(R.id.imageView);
+
+        if(!hasCamera()){
+            btn.setEnabled(false);
+        }
 
     }
+
+    public boolean hasCamera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CAMERA_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap pic = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(pic);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void capture(View v){
+
+        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_CAMERA_CAPTURE) ;
+
+    }
+
+
 }
